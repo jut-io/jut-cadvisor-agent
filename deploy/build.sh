3558e9fa62e9a71cf8d9cefc2a0afbc6ba8ecfaa
Mark Stemm <mstemm@jut.io>
2015-09-23 10:22:40 -0700

    PROD-9734 Include CA certificates in docker image.
    Modify the build.sh script to copy the ssl certificates list from
    /etc/ssl/certs to the current directory. This is needed as the ADD
    command only uses absolute paths.
    
    Modify the Dockerfile to copy to /etc/ssl/certs within the image.

--- deploy/build.sh
+++ deploy/build.sh
@@ -7,4 +7,9 @@ set -x
 
 godep go build -a github.com/jut-io/jut-cadvisor-agent
 
+# jut-cadvisor-agent is looking for a file called ca-certificates.crt,
+# hence the rename. The Dockerfile will additionally copy it into an
+# appropriate location for the go program.
+cp /etc/ssl/certs/ca-bundle.crt ./ca-certificates.crt
+
 sudo docker build -t jutd/jut-cadvisor-agent:latest .
